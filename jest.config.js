dac187200d0e81cbec9cadd36733f80ea68c6e98
apicomplexa <aleksadr.kulish@gmail.com>
2025-11-04 21:53:08 +0300

    test: Fix Jest configuration
    - Fix path aliases in Jest config same as tsconfig.
    - Set ./src as jest rootDir (following to tsconfig).
    - Set custom command (with nvs) to run jest

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,11 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
   moduleFileExtensions: ['js', 'ts', 'json', 'node'],
-  moduleNameMapper: { '@/(.*)': '<rootDir>/src/$1' },
+  moduleNameMapper: {
+    '#/(.*)': '<rootDir>/src/$1',
+    '#types/(.*)': '<rootDir>/src/types/$1',
+    '#tools/(.*)': '<rootDir>/src/tools/$1',
+  },
   testMatch: ['**/src/**/*.test.(js|ts)'],
   transform: {
     ...tsJestTransformCfg,
