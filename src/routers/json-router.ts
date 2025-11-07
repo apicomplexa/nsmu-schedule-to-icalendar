12b5c4118b0cf9efedb856adde5ca05be6a19f24
apicomplexa <aleksadr.kulish@gmail.com>
2025-11-07 21:42:10 +0300

    refactor: :recycle: Wrap all logic of loading and parsing lessons to one function
    Also specift .env variable NSMU_BASE_URL containing URL of NSMU web schedule

--- src/routers/json-router.ts
+++ src/routers/json-router.ts
@@ -1,20 +1,17 @@
 import express from 'express'
-import { NsmuWebLoader, WebScheduleParser } from '#/scraper'
+import { getLessons } from '#/scraper'
 
-export const JsonRouterFabric = (nsmuBaseURL: string): express.Router => {
+export const JsonRouterFabric = (): express.Router => {
   const router = express.Router()
-  const webLoader = new NsmuWebLoader(nsmuBaseURL)
-  const parser = new WebScheduleParser()
 
   router.get('/:curse/:group/:spec', async (req, res) => {
     console.log(
       `Request ${req.params.curse}/${req.params.group}/${req.params.spec}`
     )
-    const htmlSchedule = await webLoader.loadSchedule({
-      group: `${req.params.curse}/${req.params.group}`,
-      spec: req.params.spec,
-    })
-    const lessons = parser.parseWebSchedule(htmlSchedule)
+    const lessons = await getLessons(
+      `${req.params.curse}/${req.params.group}`,
+      req.params.spec
+    )
     res.json(lessons)
   })
 
--- src/scraper/index.ts
+++ src/scraper/index.ts
@@ -1,2 +1,22 @@
-export { NsmuWebLoader } from './web-schedule-loader'
-export { WebScheduleParser } from './web-schedule-parser'
+import { ILesson } from '#/types/lesson'
+import { NsmuWebLoader } from './web-schedule-loader'
+import { WebScheduleParser } from './web-schedule-parser'
+
+const nsmuBaseURL =
+  process.env.NSMU_BASE_URL ??
+  'https://ruz.nsmu.ru/?week={week}&group={group}&spec={spec}'
+
+const webLoader = new NsmuWebLoader(nsmuBaseURL)
+const parser = new WebScheduleParser()
+
+export async function getLessons(
+  group: string,
+  spec: string
+): Promise<ILesson[]> {
+  const htmlSchedule = await webLoader.loadSchedule({
+    group,
+    spec,
+  })
+  const lessons = parser.parseWebSchedule(htmlSchedule)
+  return lessons
+}
