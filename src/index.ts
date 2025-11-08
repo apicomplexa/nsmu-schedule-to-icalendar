96205df32303a70e2c8f5d261f157eb7dc40f52c
apicomplexa <aleksadr.kulish@gmail.com>
2025-11-08 08:53:29 +0300

    feat: Update JSON router. Now it provides filterer lessons

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,11 @@ import { JsonRouterFabric } from './routers/json-router'
 const app = express()
 const port = Number(process.env.EXPRESS_PORT) || 3000
 
-const nsmuBaseUrl = 'https://ruz.nsmu.ru/?week={week}&group={group}&spec={spec}'
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.use('/json', JsonRouterFabric(nsmuBaseUrl))
+app.use('/json', JsonRouterFabric())
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port.toString()}`)
--- src/routers/json-router.ts
+++ src/routers/json-router.ts
@@ -1,19 +1,53 @@
 import express from 'express'
-import { getLessons } from '#/scraper'
+import { getLections, getLessons, UrlArgs } from '#/scraper'
+import { ILesson } from '#types/lesson'
+
+const lessonsMiddlewareFactory = (
+  getLessonsFunc: (params: UrlArgs) => Promise<ILesson[]>
+) => {
+  return async (
+    req: express.Request<{
+      curse: string
+      group: string
+      spec: string
+    }>,
+    res: express.Response,
+    next: () => unknown
+  ) => {
+    const lessons = await getLessonsFunc({
+      group: `${req.params.curse}/${req.params.group}`,
+      spec: req.params.spec,
+    })
+    res.json(lessons)
+    next()
+  }
+}
+
+const logMiddleware = (
+  req: express.Request<{
+    curse: string
+    group: string
+    spec: string
+  }>,
+  res: express.Response,
+  next: () => unknown
+) => {
+  console.log('Request', req.params)
+  next()
+}
 
 export const JsonRouterFabric = (): express.Router => {
   const router = express.Router()
 
-  router.get('/:curse/:group/:spec', async (req, res) => {
-    console.log(
-      `Request ${req.params.curse}/${req.params.group}/${req.params.spec}`
-    )
-    const lessons = await getLessons(
-      `${req.params.curse}/${req.params.group}`,
-      req.params.spec
-    )
-    res.json(lessons)
-  })
+  router.get('/all/:curse/:group/:spec', [
+    logMiddleware,
+    lessonsMiddlewareFactory(getLessons),
+  ])
+
+  router.get('/lections/:curse/:group/:spec', [
+    logMiddleware,
+    lessonsMiddlewareFactory(getLections),
+  ])
 
   return router
 }
