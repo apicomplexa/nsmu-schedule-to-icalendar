ab9e9ac6d2ca810971a7763f2c6fc386321d19b4
apicomplexa <aleksadr.kulish@gmail.com>
2025-11-08 09:36:53 +0300

    refactor: :truck: Organise providers to one directory

--- src/routers/json-router.ts
+++ src/routers/json-router.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getLections, getLessons, UrlArgs } from '#/scraper'
+import { getLections, getLessons, UrlArgs } from '#/providers/scraper'
 import { ILesson } from '#types/lesson'
 
 const lessonsMiddlewareFactory = (
